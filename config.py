aa7ccae9dfc6ce473686e5520fcf0eb226ffa964
Nicolas Lecaros <nlecaros1@uc.cl>
2021-04-23 19:18:31 -0400

    Update url

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ class ProductiomConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_DATABASE_URI']
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    BASE_URL = 'https://iic3103-t2.herokuapp.com/'
+    BASE_URL = 'https://iic3103-t2.herokuapp.com'
 
 
 config = {
